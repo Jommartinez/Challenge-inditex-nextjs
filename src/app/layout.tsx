e02b3c536db2c643944c5e12d1f241463aede90a
Jonathan Martínez <jommartinez@MacBook-Pro---Jonathan.local>
2024-08-02 13:19:38 +0200

    feat: base project

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Roboto_Condensed } from 'next/font/google'
+import './globals.css'
+
+const Roboto = Roboto_Condensed({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Inditex Challenge',
+  description: 'Challenge for Inditex - Jonathan Martinez',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={Roboto.className}>{children}</body>
+    </html>
+  )
+}
